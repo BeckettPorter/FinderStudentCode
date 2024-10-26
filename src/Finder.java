74bc62aa7b5c3b1ef933568bd64935a1f26b8504
BeckettPorter <porterbeckett@gmail.com>
2024-10-25 17:29:00 -0700

    init

--- src/Finder.java
+++ src/Finder.java
@@ -6,17 +6,21 @@ import java.io.IOException;
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Beckett Porter
  **/
 
 public class Finder {
 
     private static final String INVALID = "INVALID KEY";
+    private static final int p = 999983;
 
     public Finder() {}
 
-    public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
+    public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException
+    {
         // TODO: Complete the buildTable() function!
+        // Make array of like 1 mil (near p value) of arraylists, then store each match in that arraylist and if
+        // there are multiple entries (ie, collisions) then go through and check each thing individually.
         br.close();
     }
 
