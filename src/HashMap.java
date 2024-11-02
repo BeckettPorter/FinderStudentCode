4bf971bbf39359a26300625508022bbdb25c9710
BeckettPorter <porterbeckett@gmail.com>
2024-11-02 11:06:33 -0700

    Refactored and optimized code

--- src/HashMap.java
+++ src/HashMap.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class HashMap
 {
-    private static final int DEFAULT_TABLE_SIZE = 5;
+    private static final int DEFAULT_TABLE_SIZE = 100000;
     private static final int RADIX = 256;
     private static final String INVALID = "INVALID KEY";
     private int tableSize;
@@ -17,22 +17,35 @@ public class HashMap
         totalFilledSlots = 0;
     }
 
+    // Search through the array to find a given key, return INVALID if not found.
     public String findValue(String key)
     {
-        int currentIndex = hashSingleString(key);
+        int keyLocation = findKeyInTable(key);
 
-
-        while (hashMap[currentIndex] != null && !hashMap[currentIndex].getKey().equals(key))
-        {
-            currentIndex = (currentIndex + 1) % tableSize;
-        }
-        if (hashMap[currentIndex] == null)
+        if (keyLocation == -1)
         {
             return INVALID;
         }
-        return hashMap[currentIndex].getValue();
+        return hashMap[keyLocation].getValue();
     }
 
+    // Find the index of a key in the table, return -1 if not found.
+    private int findKeyInTable(String key)
+    {
+        int currentIndex = hashSingleString(key);
+
+        while (hashMap[currentIndex] != null)
+        {
+            if (hashMap[currentIndex].getKey().equals(key))
+            {
+                return currentIndex;
+            }
+            currentIndex = (currentIndex + 1) % tableSize;
+        }
+        return -1;
+    }
+
+    // Add a key value pair to the array, adding it to the previously hashed arrayList if it wasn't already hashed.
     public void addPair(String key, String value, boolean wasPreviouslyHashed)
     {
         // Check if the table needs to be increased in size.
@@ -41,23 +54,16 @@ public class HashMap
             resizeTable(tableSize * 2);
         }
 
-        int currentProbeIndex = hashSingleString(key);
-        // While the location isn't empty, keep checking for an open spot.
-        while (hashMap[currentProbeIndex] != null)
-        {
-            currentProbeIndex = (currentProbeIndex + 1) % tableSize;
-        }
-
         KeyValueEntry keyToAdd = new KeyValueEntry(key, value);
-        hashMap[currentProbeIndex] = keyToAdd;
+        hashMap[findOpenTableLocation(key)] = keyToAdd;
 
         if (!wasPreviouslyHashed)
         {
             previouslyHashed.add(keyToAdd);
         }
-
     }
 
+    // Method to resize the table array and rehash previously added values.
     private void resizeTable(int newSize)
     {
         hashMap = new KeyValueEntry[newSize];
@@ -67,6 +73,21 @@ public class HashMap
         {
             addPair(pair.getKey(), pair.getValue(), true);
         }
+    }
+
+    // Find the next open table location given a key.
+    private int findOpenTableLocation(String key)
+    {
+        // Search until an empty spot is found to add the new hash.
+        int currentProbeIndex = hashSingleString(key);
+
+        // While the location isn't empty, keep checking for an open spot.
+        while (hashMap[currentProbeIndex] != null)
+        {
+            currentProbeIndex = (currentProbeIndex + 1) % tableSize;
+        }
+
+        return currentProbeIndex;
     }
 
     // Helper method that hashes a single string using the Rabin-Karp algorithm.
